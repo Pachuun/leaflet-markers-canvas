b0ecf8b66dda794afb21d28edfedfb3ca189ce94
akorobochkin <a.korobochkin@sigma-it.ru>
2022-08-18 19:20:25 +0300

    test

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,22 @@
-import buble from "@rollup/plugin-buble";
-import { uglify } from "rollup-plugin-uglify";
+import { uglify } from 'rollup-plugin-uglify';
+
+import buble from '@rollup/plugin-buble';
+import commonjs from '@rollup/plugin-commonjs';
 
 export default {
-  input: "src/leaflet-markers-canvas.js",
-  output: [
-    {
-      file: "dist/leaflet-markers-canvas.js",
-      format: "umd",
-    },
-    {
-      file: "dist/leaflet-markers-canvas.min.js",
-      format: "umd",
-      plugins: [uglify()],
-    },
-  ],
-  plugins: [buble()],
-  external: ["rbush", "leaflet"],
+    input: "src/leaflet-markers-canvas.js",
+    output: [
+        {
+            file: "dist/leaflet-markers-canvas.js",
+            format: "umd",
+        },
+        {
+            file: "dist/leaflet-markers-canvas.min.js",
+            format: "umd",
+            plugins: [uglify()],
+        },
+    ],
+    plugins: [buble(),
+    commonjs({ transformMixedEsModules: true })],
+    external: ["rbush", "leaflet", "react-svg-to-image"],
 };
